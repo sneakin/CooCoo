94fd10beba0dc58557da673ac00219a873d09dd4
Nolan Eakins <sneakin@semanticgap.com>
2018-01-23 12:57:49 -0500

    Renamed some variables in the mnist_dreamer.

--- examples/mnist_dream.rb
+++ examples/mnist_dream.rb
@@ -47,12 +47,13 @@ end
 def sgd(opts)
   f = opts.fetch(:f)
   cost = opts.fetch(:cost)
-  loss = opts.fetch(:loss)
+  delta = opts.fetch(:delta)
   update = opts.fetch(:update)
   #on_batch = opts.fetch(:on_batch)
   status = opts.fetch(:status)
   epochs = opts.fetch(:epochs, 1)
   rate = opts.fetch(:rate)
+  momentum = opts.fetch(:momentum, 0.0)
   verbose = opts.fetch(:verbose, false)
   status_time = opts.fetch(:status_time, Float::INFINITY)
 
@@ -65,8 +66,8 @@ def sgd(opts)
   epochs.times do |e|
     output = f.call()
     c = cost.call(*output)
-    deltas = loss.call(c, *output) * rate
-    update.call(deltas, last_deltas * rate)
+    deltas = delta.call(c, *output) * rate - last_deltas * momentum
+    update.call(deltas)
     last_deltas = deltas
     dt = Time.now - last_time
     if status && verbose && dt > status_time
@@ -90,27 +91,27 @@ def sgd(opts)
   end
 end
 
-def backprop_digit(loops, rate, net, digit, initial_input = nil, verbose = false, status_delay = 5.0, to_ascii = true, to_sixel = false)
+def backprop_digit(loops, rate, momentum, net, digit, initial_input = nil, verbose = false, status_delay = 5.0, to_ascii = true, to_sixel = false)
   initial_input ||= CooCoo::Vector.zeros(net.num_inputs)
   input = initial_input
   target = CooCoo::Vector.zeros(net.num_outputs)
   target[digit % net.num_outputs] = 1.0
   target = net.prep_output_target(target)
 
-  sgd(epochs: loops, rate: rate, status_time: status_delay, verbose: verbose,
+  sgd(epochs: loops, rate: rate, momentum: momentum, status_time: status_delay, verbose: verbose,
       f: lambda do
         output, hs = net.forward(input, {}, true, true)
       end,
       cost: lambda do |output, hs|
         output.last - target
       end,
-      loss: lambda do |c, output, hs|
+      delta: lambda do |c, output, hs|
         deltas, hs = net.backprop(input, output, c, hs)
         errs = net.transfer_errors(deltas)
         x = errs.first
       end,
-      update: lambda do |deltas, last_deltas|
-        input = input - deltas + last_deltas
+      update: lambda do |deltas|
+        input = input - deltas
       end,
       status: lambda do |opts|
         puts("#{opts[:epoch]} #{digit} Input")
@@ -128,6 +129,7 @@ options = OpenStruct.new
 options.model_path = nil
 options.loops = 10
 options.rate = 0.5
+options.momentum = 0.1
 options.initial_input = CooCoo::Vector.zeros(28 * 28)
 options.status_delay = 5.0
 options.ascii = true
@@ -162,6 +164,10 @@ opts = CooCoo::OptionParser.new do |o|
     options.rate = n.to_f
   end
 
+  o.on('--momentum NUMBER') do |n|
+    options.momentum = n.to_f
+  end
+
   o.on('-v', '--verbose') do
     options.verbose = true
   end
@@ -193,7 +199,7 @@ argv = 10.times if argv.empty?
 argv.collect do |digit|
   digit = digit.to_i
   $stdout.puts("Generating #{digit}") if options.verbose
-  input = backprop_digit(options.loops, options.rate, net, digit.to_i, options.initial_input, options.verbose, options.status_delay, options.ascii, options.sixel)
+  input = backprop_digit(options.loops, options.rate, options.momentum, net, digit.to_i, options.initial_input, options.verbose, options.status_delay, options.ascii, options.sixel)
   $stdout.flush
   [ digit, input ]
 end.each do |digit, input|
